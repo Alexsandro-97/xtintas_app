ca6021b7a1a3478f48a15ee430b4c82180e3cbc0
Alexsandro-97 <alexsandroeletrotecnica@gmail.com>
2022-06-14 01:33:32 -0300

    Adding Dio client

---
+++ lib/repositories/client/dio_client.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import '/repositories/client/i_client_repository.dart';
+import '/utils/constants/url_constant.dart';
+
+class DioClient implements IClient {
+  final dio = Dio();
+
+  @override
+  Future<Response> delete(String route, {dynamic data}) async {
+    final Response response =
+        await dio.delete(UrlsAndRoutes.baseUrl + route, data: data);
+    return response;
+  }
+
+  @override
+  Future<Response> get(String route) async {
+    dio.options.headers['Content-Type'] = 'application/json';
+    final Response response = await dio.get(UrlsAndRoutes.baseUrl + route);
+    return response;
+  }
+
+  @override
+  Future<Response> post(String route, {dynamic data}) async {
+    final Response response =
+        await dio.post(UrlsAndRoutes.baseUrl + route, data: data);
+    return response;
+  }
+
+  @override
+  Future<Response> put(String route, {dynamic data}) async {
+    final Response response =
+        await dio.put(UrlsAndRoutes.baseUrl + route, data: data);
+    return response;
+  }
+}
