632127434c40ad7789e85d789d73d69d1bbc52c1
Alexsandro-97 <alexsandroeletrotecnica@gmail.com>
2022-06-14 01:10:00 -0300

    config file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:xtintas_loomi_app/main.dart';
+import 'package:xtintas_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
