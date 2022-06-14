cd673c8eb5ae287ae66bbd3ffce409b83fe45fdc
Alexsandro-97 <alexsandroeletrotecnica@gmail.com>
2022-06-14 01:09:20 -0300

    Adding UrlAndRoutes class

---
+++ lib/utils/constants/url_constant.dart
@@ -0,0 +1,7 @@
+abstract class UrlsAndRoutes {
+  static String baseUrl = 'https://62968cc557b625860610144c.mockapi.io';
+  static String loginRoute = '/login';
+  static String signUpRoute = '/user';
+  static String paintsRoute = '/paints';
+  static String profileRoute = '/profile';
+}
