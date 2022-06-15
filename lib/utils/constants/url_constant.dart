dac3887745a4332ebea58edbc210fa2de3d14e73
Alexsandro-97 <alexsandroeletrotecnica@gmail.com>
2022-06-15 00:16:01 -0300

    adding new UrlRoute

--- lib/utils/constants/url_constant.dart
+++ lib/utils/constants/url_constant.dart
@@ -1,7 +1,8 @@
 abstract class UrlsAndRoutes {
   static String baseUrl = 'https://62968cc557b625860610144c.mockapi.io';
-  static String loginRoute = '/login';
+  static String signInRoute = '/login';
   static String signUpRoute = '/user';
+  static String signOutRoute = '/logout';
   static String paintsRoute = '/paints';
   static String profileRoute = '/profile';
 }
