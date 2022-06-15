ac7bbdd27aae552466f8351b2d0abb803ef4d80e
Alexsandro-97 <alexsandroeletrotecnica@gmail.com>
2022-06-15 15:18:58 -0300

    updated cartScreen

--- lib/view/screens/cart_screen.dart
+++ lib/view/screens/cart_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/card_cart_widget.dart';
+
 class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
 
@@ -10,8 +12,35 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Cart Screen')),
+    final sizeScreen = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Carrinho',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: ListView.separated(
+          separatorBuilder: (context, index) => const SizedBox(height: 10),
+          itemCount: 3,
+          itemBuilder: (context, index) => CardCart(
+            sizeScreen: sizeScreen,
+            title: 'Tinta Suvinil Criativa',
+            unit: 1,
+            price: 95,
+          ),
+        ),
+      ),
     );
   }
 }
