e87b61fec2ef340e77412003368726ce65328910
Alexsandro-97 <alexsandroeletrotecnica@gmail.com>
2022-06-14 21:16:33 -0300

    adding new color

--- lib/utils/colors/app_colors.dart
+++ lib/utils/colors/app_colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static Color backgroundSignInColor = const Color(0xFF5B4DA7);
+  static Color cursorColor = const Color(0xFFFFFFFF).withOpacity(0.5);
 }
