7303b3ddc11d68de065d2b519d3e4d1fe001997c
Alexsandro-97 <alexsandroeletrotecnica@gmail.com>
2022-06-15 18:09:49 -0300

    adding data api to cartScreen

--- lib/view/screens/cart_screen.dart
+++ lib/view/screens/cart_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:lottie/lottie.dart';
+import 'package:xtintas_app/controller/stores/cart_store.dart';
 
 import '../widgets/card_cart_widget.dart';
 
@@ -10,6 +13,14 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  CartStore cartStore = CartStore();
+
+  @override
+  void initState() {
+    cartStore.getCarts();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final sizeScreen = MediaQuery.of(context).size;
@@ -28,19 +39,27 @@ class _CartScreenState extends State<CartScreen> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: ListView.separated(
-          separatorBuilder: (context, index) => const SizedBox(height: 10),
-          itemCount: 3,
-          itemBuilder: (context, index) => CardCart(
-            sizeScreen: sizeScreen,
-            title: 'Tinta Suvinil Criativa',
-            unit: 1,
-            price: 95,
-          ),
-        ),
-      ),
+      body: Observer(builder: (context) {
+        return cartStore.carts.isNotEmpty
+            ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: ListView.separated(
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(height: 10),
+                  itemCount: cartStore.carts.length,
+                  itemBuilder: (context, index) => CardCart(
+                    sizeScreen: sizeScreen,
+                    src: cartStore.carts[index].paint!.image!,
+                    title: cartStore.carts[index].paint!.name!,
+                    unit: cartStore.carts[index].quantity!,
+                    price: cartStore.carts[index].paint!.price!,
+                  ),
+                ),
+              )
+            : Center(
+                child: Lottie.network(
+                    'https://assets7.lottiefiles.com/packages/lf20_sjcbakkb.json'));
+      }),
     );
   }
 }
